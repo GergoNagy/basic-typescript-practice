7dabb099daef21002020944524f5e9351bf5d4e7
Gergo Nagy <guzmop@gmail.com>
2022-07-13 09:34:17 +0200

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+//Basic sintact of TypeScript
+let id = 5;
+id = 5;
+// Run a tsc file
+//-----------------
+//Compile tsc with:             tsc index
+//To whatch any changes:        tsc --watch index 
+//Set up a tsc config file
+//-----------------
+//Set up a tsc config file:     tsc --init
+// With config file run "tsc" it compile all the ts files in the project
+// Can specfy where to compile the JS files. Itt will be in a different folder (pl dist)
+// Output the compliled files:  outDir: "./dist"
+// Where to find the ts files:  rootDir: "./src"
